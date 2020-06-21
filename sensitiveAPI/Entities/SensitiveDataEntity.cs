c4a3fbd77507929253cb503163541e22a4c565e4
Amin Shojaatmand <amin.shojaatmand@gmail.com>
2020-06-21 18:32:38 +0200

    Create entity model

---
+++ sensitiveAPI/Entities/SensitiveDataEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sensitiveAPI.Entities
+{
+    public class SensitiveDataEntity
+    {
+        /// <summary>
+        /// UID created in code
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual long Id { get; set; }
+
+        /// <summary>
+        /// This is AesModel in Json Format 
+        /// </summary>
+        public virtual string Data { get; set; }
+
+        /// <summary>
+        /// The name of the encryption key used
+        /// when encrypting this data
+        /// </summary>
+        public virtual string EncryptionKeyName { get; set; }
+    }
+}
